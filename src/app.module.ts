cd84554052c79975c0fcf3950a35615751e3470a
Paige Zaleppa <pz0342@gmail.com>
2022-04-18 17:41:54 -0400

    :fire: Starting fresh module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CalendarModule } from './calendar/calendar.module';
-import { DiscussionModule } from './discussion/discussion.module';
-import { GradeModule } from './grade/grade.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CalendarModule, DiscussionModule, GradeModule, UserModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
