0e174e64ff96e7e76b94af254dfbc5634ab1a613
Paige Zaleppa <pz0342@gmail.com>
2022-06-04 07:53:35 -0400

    :sparkles: Finish schemas

--- src/entities/setting/setting.ts
+++ src/entities/setting/setting.ts
@@ -1,11 +1,30 @@
+import { Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+
+export type SettingDocument = Setting & Document;
+
+@Schema()
 export class Setting {
-    public id: string;
+    @Prop(Types.ObjectId)
+    public id: Types.ObjectId;
+
+    @Prop(String)
     public starter_prompt: string;
-    public post_inspiration: string;
-    public score: string;
-    public calendar: string;
+
+    @Prop(Types.ObjectId)
+    public post_inspiration: Types.ObjectId;
+
+    @Prop(Types.ObjectId)
+    public score: Types.ObjectId;
+
+    @Prop(Types.ObjectId)
+    public calendar: Types.ObjectId;
     
     constructor(partial: Partial<Setting>) {
         Object.assign(this, partial);
     }
 }
+
+export const SettingSchema = SchemaFactory.createForClass(Setting);
