c4d9338d26e30d8b22f116195c4c41430d9d19ca
Paige Zaleppa <pz0342@gmail.com>
2022-07-22 21:44:49 -0400

    Comment out post inspiration post, patch, and delete

--- src/modules/inspiration/inspiration.controller.ts
+++ src/modules/inspiration/inspiration.controller.ts
@@ -14,18 +14,18 @@ export class InspirationController {
     @InjectModel(Inspiration.name) private inspirationModel: Model<Inspiration>,
   ) {}
 
-  @Post('inspiration')
-  @ApiOperation({description: 'Create an inspiration for a discussion'})
-  @ApiBody({description: 'Inspiration to create', type: InspirationCreateDTO})
-  @ApiOkResponse({ description: 'Inspiration created!'})
-  @ApiBadRequestResponse({ description: ''})
-  @ApiUnauthorizedResponse({ description: ''})
-  @ApiNotFoundResponse({ description: ''})
-  @ApiTags('Inspiration')
-  async createInspiration(@Body() inspiration: InspirationCreateDTO): Promise<Inspiration> {
-    const createdInspiration = new this.inspirationModel(inspiration);
-    return await createdInspiration.save();
-  }
+  // @Post('inspiration')
+  // @ApiOperation({description: 'Create an inspiration for a discussion'})
+  // @ApiBody({description: 'Inspiration to create', type: InspirationCreateDTO})
+  // @ApiOkResponse({ description: 'Inspiration created!'})
+  // @ApiBadRequestResponse({ description: ''})
+  // @ApiUnauthorizedResponse({ description: ''})
+  // @ApiNotFoundResponse({ description: ''})
+  // @ApiTags('Inspiration')
+  // async createInspiration(@Body() inspiration: InspirationCreateDTO): Promise<Inspiration> {
+  //   const createdInspiration = new this.inspirationModel(inspiration);
+  //   return await createdInspiration.save();
+  // }
 
   @Get('inspirations')
   @ApiOperation({description: 'Gets all valid inspirations on the system'})
@@ -40,42 +40,42 @@ export class InspirationController {
     return { posting, responding, synthesizing };
   }
 
-  @Patch('inspiration/:inspirationId')
-  @ApiOperation({description: 'Update a post inspiration'})
-  @ApiBody({description: 'The Inspiration to update', type: InspirationEditDTO})
-  @ApiParam({name: 'inspirationId', description: 'The id of the inspiration'})
-  @ApiOkResponse({ description: 'Inspiration updated!'})
-  @ApiBadRequestResponse({ description: 'Inspiration Id is invalid'})
-  @ApiUnauthorizedResponse({ description: ''})
-  @ApiNotFoundResponse({ description: ''})
-  @ApiTags('Inspiration')
-  async updateInspiration(@Param('inspirationId') inspirationId: string, @Body() inspiration: InspirationEditDTO): Promise<any> {
-    if(!Types.ObjectId.isValid(inspirationId)) {
-      throw new HttpException('The inspirationId is not valid', HttpStatus.BAD_REQUEST);
-    }
-    const foundInspiration = await this.inspirationModel.findOne({ _id: inspirationId });
-    if(!foundInspiration) {
-      throw new HttpException('The inspiration does not exist', HttpStatus.NOT_FOUND);
-    }
-    return await this.inspirationModel.findOneAndUpdate({_id: inspirationId}, inspiration);
-  }
+  // @Patch('inspiration/:inspirationId')
+  // @ApiOperation({description: 'Update a post inspiration'})
+  // @ApiBody({description: 'The Inspiration to update', type: InspirationEditDTO})
+  // @ApiParam({name: 'inspirationId', description: 'The id of the inspiration'})
+  // @ApiOkResponse({ description: 'Inspiration updated!'})
+  // @ApiBadRequestResponse({ description: 'Inspiration Id is invalid'})
+  // @ApiUnauthorizedResponse({ description: ''})
+  // @ApiNotFoundResponse({ description: ''})
+  // @ApiTags('Inspiration')
+  // async updateInspiration(@Param('inspirationId') inspirationId: string, @Body() inspiration: InspirationEditDTO): Promise<any> {
+  //   if(!Types.ObjectId.isValid(inspirationId)) {
+  //     throw new HttpException('The inspirationId is not valid', HttpStatus.BAD_REQUEST);
+  //   }
+  //   const foundInspiration = await this.inspirationModel.findOne({ _id: inspirationId });
+  //   if(!foundInspiration) {
+  //     throw new HttpException('The inspiration does not exist', HttpStatus.NOT_FOUND);
+  //   }
+  //   return await this.inspirationModel.findOneAndUpdate({_id: inspirationId}, inspiration);
+  // }
 
-  @Delete('inspiration/:inspirationId')
-  @ApiOperation({description: 'Deletes a specified inspiration'})
-  @ApiParam({name: 'inspirationId', description: 'The id of the inspiration that needs to be added'})
-  @ApiOkResponse({ description: 'Inspiration Deleted'})
-  @ApiBadRequestResponse({ description: ''})
-  @ApiUnauthorizedResponse({ description: ''})
-  @ApiNotFoundResponse({ description: ''})
-  @ApiTags('Inspiration')
-  async deleteInspiration(@Param('inspirationId') inspirationId: string): Promise<any> {
-    if(!Types.ObjectId.isValid(inspirationId)) {
-      throw new HttpException('The inspirationId is not valid', HttpStatus.BAD_REQUEST);
-    }
-    const foundInspiration = await this.inspirationModel.findOne({ _id: inspirationId });
-    if(!foundInspiration) {
-      throw new HttpException('The inspiration does not exist', HttpStatus.NOT_FOUND);
-    }
-    return await this.inspirationModel.findOneAndDelete({_id: new Types.ObjectId(inspirationId)});
-  }
+  // @Delete('inspiration/:inspirationId')
+  // @ApiOperation({description: 'Deletes a specified inspiration'})
+  // @ApiParam({name: 'inspirationId', description: 'The id of the inspiration that needs to be added'})
+  // @ApiOkResponse({ description: 'Inspiration Deleted'})
+  // @ApiBadRequestResponse({ description: ''})
+  // @ApiUnauthorizedResponse({ description: ''})
+  // @ApiNotFoundResponse({ description: ''})
+  // @ApiTags('Inspiration')
+  // async deleteInspiration(@Param('inspirationId') inspirationId: string): Promise<any> {
+  //   if(!Types.ObjectId.isValid(inspirationId)) {
+  //     throw new HttpException('The inspirationId is not valid', HttpStatus.BAD_REQUEST);
+  //   }
+  //   const foundInspiration = await this.inspirationModel.findOne({ _id: inspirationId });
+  //   if(!foundInspiration) {
+  //     throw new HttpException('The inspiration does not exist', HttpStatus.NOT_FOUND);
+  //   }
+  //   return await this.inspirationModel.findOneAndDelete({_id: new Types.ObjectId(inspirationId)});
+  // }
 }
