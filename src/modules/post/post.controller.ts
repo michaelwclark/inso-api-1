1797fc1fc4bb3d5ce39c492d48c7253ded5281ae
Paige Zaleppa <pz0342@gmail.com>
2022-07-23 09:46:15 -0400

    Adding updates and tests

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ApiOperation, ApiBody, ApiOkResponse, ApiBadRequestResponse, ApiUnauthorizedResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
 import { Model, Types } from 'mongoose';
 import { Discussion, DiscussionDocument } from 'src/entities/discussion/discussion';
-import { InspirationCreateDTO } from 'src/entities/inspiration/create-inspiration';
+import { Inspiration, InspirationDocument } from 'src/entities/inspiration/inspiration';
 import { PostCreateDTO } from 'src/entities/post/create-post';
 import { PostUpdateDTO } from 'src/entities/post/edit-post';
 import { DiscussionPost, DiscussionPostDocument } from 'src/entities/post/post';
+import { Setting, SettingDocument } from 'src/entities/setting/setting';
 
 
 @Controller()
 export class PostController {
   constructor(
     @InjectModel(Discussion.name) private discussionModel: Model<DiscussionDocument>,
-    @InjectModel(DiscussionPost.name) private discussionPostModel: Model<DiscussionPostDocument>
+    @InjectModel(DiscussionPost.name) private discussionPostModel: Model<DiscussionPostDocument>,
+    @InjectModel(Inspiration.name) private inspirationModel: Model<InspirationDocument>,
+    @InjectModel(Setting.name) private settingsModel: Model<SettingDocument>
   ) {}
 
   @Post('discussion/:discussionId/post')
@@ -25,7 +28,7 @@ export class PostController {
   @ApiTags('Inspiration')
   async createPost(@Param('discussionId') discussionId: string, @Body() post: PostCreateDTO): Promise<string> {
     if(!Types.ObjectId.isValid(discussionId)) {
-      throw new HttpException(`${discussionId} is not a valid discussion`, HttpStatus.BAD_REQUEST);
+      throw new HttpException(`${discussionId} is not a valid discussionId`, HttpStatus.BAD_REQUEST);
     }
     const discussion = await this.discussionModel.findOne({ _id: new Types.ObjectId(discussionId)});
     if(!discussion) {
@@ -52,12 +55,57 @@ export class PostController {
   }
 
   @Patch('discussion/:discussionId/post/:postId')
-  updatePost(@Param('discussionId') discussionId: string, @Param('postId') postId: string, @Body() postUpdates: PostUpdateDTO): string {
-    return 'updated'
+  @ApiOperation({description: 'Update a post in a discussion'})
+  @ApiBody({description: 'Post to update', type: PostUpdateDTO})
+  @ApiOkResponse({ description: 'Post updated!'})
+  @ApiUnauthorizedResponse({ description: ''})
+  @ApiNotFoundResponse({ description: 'User, Post, or discussion does not exist'})
+  @ApiTags('Inspiration')
+  async updatePost(@Param('discussionId') discussionId: string, @Param('postId') postId: string, @Body() postUpdates: PostUpdateDTO): Promise<DiscussionPost> {
+    if(!Types.ObjectId.isValid(discussionId)) {
+      throw new HttpException(`${discussionId} is not a valid discussionId`, HttpStatus.BAD_REQUEST);
+    }
+    const discussion = await this.discussionModel.findOne({ _id: new Types.ObjectId(discussionId)});
+    if(!discussion) {
+      throw new HttpException(`${discussionId} was not found`, HttpStatus.NOT_FOUND);
+    }
+
+    if(!Types.ObjectId.isValid(postId)) {
+      throw new HttpException(`${discussionId} is not a valid postId`, HttpStatus.BAD_REQUEST);
+    }
+
+    // Check the post_inspiration is valid
+    const inspiration = await this.inspirationModel.findOne({ _id: postUpdates.post_inspiration});
+    if(!inspiration) {
+      throw new HttpException(`${postUpdates.post_inspiration} is not a valid post inspiration`, HttpStatus.NOT_FOUND);
+    }
+
+    // Check if the post inspiration is in the discussion settings inspirations array
+    const settings = await this.settingsModel.findOne({ _id: discussion.settings });
+    if(!settings.inspiration.includes(inspiration._id)) {
+      throw new HttpException(`${inspiration._id} is not an inspiration for this discussion`, HttpStatus.BAD_REQUEST);
+    }
+
+    return await this.discussionPostModel.findOneAndUpdate({ _id: new Types.ObjectId(postId)}, { postUpdates });
   }
 
   @Patch('discussion/:discussionId/post/:postId/publish')
-  publishPost(@Param('discussionId') discussionId: string, @Param('postId') postId: string): string {
+  @ApiOperation({description: 'Publish a draft post in a discussion'})
+  @ApiOkResponse({ description: 'Post published!'})
+  @ApiUnauthorizedResponse({ description: ''})
+  @ApiNotFoundResponse({ description: 'User, Post, or discussion does not exist'})
+  @ApiTags('Inspiration')
+  async publishPost(@Param('discussionId') discussionId: string, @Param('postId') postId: string): Promise<string> {
+    return 'publish from draft'
+  }
+
+  @Delete('discussion/:discussionId/post/:postId')
+  @ApiOperation({description: 'Publish a draft post in a discussion'})
+  @ApiOkResponse({ description: 'Post published!'})
+  @ApiUnauthorizedResponse({ description: ''})
+  @ApiNotFoundResponse({ description: 'User, Post, or discussion does not exist'})
+  @ApiTags('Inspiration')
+  async deletePost(@Param('discussionId') discussionId: string, @Param('postId') postId: string): Promise<string> {
     return 'publish from draft'
   }
 }
--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Discussion, DiscussionSchema } from 'src/entities/discussion/discussion';
+import { Inspiration, InspirationSchema } from 'src/entities/inspiration/inspiration';
 import { Milestone, MilestoneSchema } from 'src/entities/milestone/milestone';
 import { Notification, NotificationSchema } from 'src/entities/notification/notification';
 import { DiscussionPost, DiscussionPostSchema } from 'src/entities/post/post';
@@ -11,7 +12,8 @@ import { PostController } from './post.controller';
         MongooseModule.forFeature([{ name: DiscussionPost.name, schema: DiscussionPostSchema }]),
         MongooseModule.forFeature([{ name: Discussion.name, schema: DiscussionSchema }]),
         MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }]),
-        MongooseModule.forFeature([{ name: Milestone.name, schema: MilestoneSchema }])
+        MongooseModule.forFeature([{ name: Milestone.name, schema: MilestoneSchema }]),
+        MongooseModule.forFeature([{ name: Inspiration.name, schema: InspirationSchema }])
     ],
     controllers: [PostController],
     providers: [],
