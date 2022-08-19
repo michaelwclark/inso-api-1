d465a5365546425abc159ae3b9f4fb2b89d82912
Paige Zaleppa <pz0342@gmail.com>
2022-08-18 21:53:59 -0400

    Trying different CORS config

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api', app, document);
   
-  var whitelist = ['https://inso.ai', 'https://inso.ai', 'http://localhost:3001', /\.inso.ai$/, 'http://inso-staging.s3-website-us-east-1.amazonaws.com/'];
+  var whitelist = ['https://inso.ai', 'http://inso.ai', 'http://localhost:3001', /\.inso.ai$/, 'http://inso-staging.s3-website-us-east-1.amazonaws.com'];
   app.enableCors({
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
