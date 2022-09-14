2cc48bcfb1a5e808af1dd08e7b825b6a30c1896b
Paige Zaleppa <pz0342@gmail.com>
2022-09-14 19:36:06 -0400

    Add reaction schema

--- src/entities/reaction/create-reaction.ts
+++ src/entities/reaction/create-reaction.ts
@@ -1,13 +1,30 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDefined, IsMongoId, IsString } from "class-validator";
 import { Types } from "mongoose";
-import * as nodeEmoji from 'node-emoji';
 
 export class CreateReactionDTO {
+    
+    @ApiProperty({
+        name: 'userId',
+        description: 'The mongo id of the user',
+        required: false,
+        type: String,
+        isArray: false,
+        example: '507f1f77bcf86cd799439011'
+    })
     @IsMongoId()
     @IsDefined()
     userId: Types.ObjectId;
 
+    @ApiProperty({
+        name: 'reaction',
+        description: 'The emoji code for the reaction',
+        required: false,
+        type: String,
+        isArray: false,
+        example: '+1'
+    })
     @IsDefined()
     @IsString()
     reaction: string;
