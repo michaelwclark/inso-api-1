d338bdc002e84887c362b6dfc9dd176c517da923
Paige Zaleppa <pz0342@gmail.com>
2022-09-24 08:57:15 -0400

    Fix reset password

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -9,7 +9,6 @@ export class JwtStrategy extends PassportStrategy(Strategy){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            //secretOrKey: process.env.SECRET
             secretOrKey: jwtConstants.secret
         });
     }
