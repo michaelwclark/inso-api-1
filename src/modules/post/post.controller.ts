f222954e6fdb0e11a9baed0b29249332c7158528
Paige Zaleppa <pz0342@gmail.com>
2022-11-24 10:07:32 -0500

    :fire: remove grade service

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -45,7 +45,7 @@ import { NotificationService } from '../notification/notification.service';
 import environment from 'src/environment';
 import { Reaction, ReactionDocument } from 'src/entities/reaction/reaction';
 import POST_ERRORS from './post-errors';
-import { GradeService } from 'src/modules/grade/grade.service';
+//import { GradeService } from 'src/modules/grade/grade.service';
 
 const Filter = require('bad-words'); // eslint-disable-line
 
@@ -63,7 +63,7 @@ export class PostController {
     @InjectModel(Reaction.name) private reactionModel: Model<ReactionDocument>,
     private notificationService: NotificationService,
     private milestoneService: MilestoneService,
-    private gradeService: GradeService,
+    //private gradeService: GradeService,
   ) { }
 
   @Post('discussion/:discussionId/post')
